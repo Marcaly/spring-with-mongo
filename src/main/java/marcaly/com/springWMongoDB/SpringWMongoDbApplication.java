ff23a0ca483a8299222c072051c07e70a6cdd497
Marçal <byelmarcal@hotmail.com>
2023-07-16 11:26:24 -0300

    first commit

---
+++ src/main/java/marcaly/com/springWMongoDB/SpringWMongoDbApplication.java
@@ -0,0 +1,13 @@
+package marcaly.com.springWMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/marcaly/com/springWMongoDB/SpringWMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package marcaly.com.springWMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
